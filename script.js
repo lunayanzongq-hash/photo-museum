921c196cef8f17cd9b8bdb0c52acd77d1d01238d
luna <luna.yanzongq@gmail.com>
2026-07-12 21:42:29 +0800

    add sidebar and desc

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 ﻿const photos = [
-  { src: "photos/1.jpg", caption: "2020年5月，鸡", category: "动物", desc: "封城时买的第一个相机，在镜头下第一次仔细看到了鸡的羽毛和色彩也很漂亮。" },
- 
-  
-  { src: "photos/4.jpg", caption: "2025年12月23日晚，房门口", category: "光影", desc: "光从门缝延伸出来。" },
- 
-{ src: "photos/6.jpg", caption: "2026年6月22日晚，手抄心经", category: "笔痕", desc: "写字的时候能清楚的知道自己的整体状态。" },
-{ src: "photos/7.jpg", caption: "2026年6月15日晚，病房四人", category: "人物", desc: "外婆的两个女儿和我。" },
-
+  { src: "photos/1.jpg", caption: "2025年12月23日晚，光影", category: "光影", desc: "蓝白色的光从门缝延伸出来，作为页面的背景图刚好合适" },
+ { src: "photos/2.jpg", caption: "2015年9月2日，和奶奶打纸牌", category: "时光", desc: "还没上小学时我就用“上大人”认了一些字，能和老人一起打纸牌" },
+  { src: "photos/3.jpg", caption: "2015年9月11日，母亲陪着我去十堰入学", category: "人物", desc: "学校太远，被子之类的行李都是自己提去的" },
+  { src: "photos/4.jpg", caption: "2016年10月3日，表哥结婚", category: "人物", desc: "那时大伯还在，姑父看着也年轻" },
+  { src: "photos/5.jpg", caption: "2016年10月6日，奶奶在门口坐着", category: "人物", desc: "每个人都有自己的孤独时刻" },
+{ src: "photos/6.jpg", caption: "2017年5月19日，奶奶对着镜头笑", category: "人物", desc: "那时对她来说手机拍照还很新奇" },
+{ src: "photos/7.jpg", caption: "2020年3月15日，鸡", category: "时光", desc: "封城在家买了第一个相机，通过镜头清晰的看到了鸡的羽毛也是如此美丽" },
+{ src: "photos/8.jpg", caption: "2020年4月13日，光影", category: "光影", desc: "用相机在菜园拍的，喜欢这种光影" },
+{ src: "photos/9.jpg", caption: "2020年5月4日，奶奶卧床几年了", category: "人物", desc: "那几年都很辛苦，面对生老病死有太多无奈，尽力而为" },
 
 
 ];
